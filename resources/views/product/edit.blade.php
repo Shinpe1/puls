c8176b5a56ace525613c6f85ba40a2e62d41e558
Shinpe1 <124227110+Shinpe1@users.noreply.github.com>
2023-09-15 01:33:09 +0900

    Update edit.blade.php

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -8,30 +8,7 @@
 @csrf
 
 <table class="table table-striped">
-<thead>
-    <tr>
-    <th>ID</th>
-    <th>商品画像</th>
-    <th>商品名</th>
-    <th>価格</th>
-    <th>在庫数</th>
-    <th>メーカー名</th>
-    <th>コメント</th>
-    </tr>
-</thead>
 
-<tbody>
-    <tr>
-    <td>{{ $product->products_id }}</td>
-    <td><img src="{{ asset('storage/images/'. $product->img_path)}}" class="img-fluid" width = "300px"></td>
-    <td>{{ $product->product_name }}</td>
-    <td>{{ $product->price }}</td>
-    <td>{{ $product->stock }}</td>
-    <td>{{ $product->company->company_name }}</td>
-    <td>{{ $product->comment }}</td>
-    </tr>
-</tbody>
-</table>
 
     <fieldset>
 
@@ -41,7 +18,7 @@
         <h5 style="color:red">{{ $errors->first('product_name') }}</h5>
     @endif
         <label for="product_name">{{ __('商品名') }}</label>
-        <input type="text" class="form-name" name="product_name" id="product_name">
+        <input type="text" class="form-name" name="product_name" id="product_name" value="{{ $product->product_name }}" required>
     </div>
 
     <div class="form-group">
@@ -50,7 +27,7 @@
         <h5 style="color:red">{{ $errors->first('price') }}</h5>
     @endif
         <label for="price">{{ __('価格') }}</label>
-        <input type="text" class="form-name" name="price" id="price">
+        <input type="text" class="form-name" name="price" id="price" value="{{ $product->price }}" required>
     </div>
 
     <div class="form-group">
@@ -59,16 +36,16 @@
         <h5 style="color:red">{{ $errors->first('stock') }}</h5>
     @endif
         <label for="stock">{{ __('在庫数') }}</label>
-        <input type="text" class="form-name" name="stock" id="stock">
+        <input type="text" class="form-name" name="stock" id="stock"value="{{ $product->stock }}" required >
     </div>
 
-    <div class="form-group">
+    <div class="mb-3">
     <div>
         @if ($errors->has('company_id'))
         <h5 style="color:red">{{ $errors->first('company_id') }}</h5>
     @endif
     <label for="company_id">{{ __('メーカー名') }}</label>
-    <select type="text" class="form-select" id="company_id" name="company_id">
+    <select type="text" class="form-select" id="company_id" name="company_id" >
     <option disabled style='display:none;' @if (empty($product->company_id)) selected @endif>選択してください</option>
     @foreach($companies as $company)
             <option value="{{ $company->company_id }}" @if (isset($product->company_id) && ($product->company_id === $company->company_id)) selected @endif>{{ $company->company_name }}</option>
@@ -77,12 +54,13 @@
         
     <div class="form-group">
         <label for="comment">{{ __('コメント') }}</label>
-        <input type="text" class="form-name" name="comment" id="comment">
+        <input type="text" class="form-name" name="comment" id="comment" value="{{ $product->comment }}" required>
     </div>
 
     <div>
             <label for="img_path">{{ __('商品画像') }}</label>
             <input type="file" class="form-" name="img_path" id="img_path">
+            <img src="{{ asset('storage/images/'. $product->img_path)}}" class="img-fluid" width = "100px">
         <div class="d-flex justify-content-between pt-3">
 </dit>
 
